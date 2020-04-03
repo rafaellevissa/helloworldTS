23efb23d0681176603092792279567371ccb96c8
rafaellevissa <rafaellevissa@gmail.com>
2020-04-03 02:08:01 -0300

    hello com interface

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # helloworldTS
 Hello world com TypeScript, seguindo a documentação: https://www.typescriptlang.org/docs
+
+* To compile:
+
+`tsc hello.ts`
---
+++ src/hello.ts
@@ -0,0 +1,12 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function hello(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Rafael", lastName: "Levi" };
+
+console.log(hello(user));
